5388b850289bb515a2b7a49109ef2da5d6a06956
ghonman2 <ghonman2@gmail.com>
2023-07-24 16:08:50 +0900

    sign in 구현(#20)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,49 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtModuleConfig } from 'src/configs/jwt.config';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './access-token.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/user.entity';
 import { UserRepository } from 'src/user/user.repository';
 import { UserService } from 'src/user/user.service';
+import { FriendService } from 'src/friend/friend.service';
+import { FriendRepository } from 'src/friend/friend.respository';
+import { BlockRepository } from 'src/block/block.repository';
+import { BlockService } from 'src/block/block.service';
+import { ChannelRepository } from 'src/channel/channel.repository';
+import { ChannelUserRepository } from 'src/channel/channel-user.repository';
+import { ChannelService } from 'src/channel/channel.service';
+import { FriendEntity } from 'src/friend/friend.entity';
+import { BlockEntity } from 'src/block/block.entity';
+import { ChannelEntity } from 'src/channel/channel.entity';
+import { ChannelUserEntity } from 'src/channel/channel-user.entity';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register(JwtModuleConfig),
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([
+      UserEntity,
+      FriendEntity,
+      BlockEntity,
+      ChannelEntity,
+      ChannelUserEntity,
+    ]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository, JwtStrategy],
+  providers: [
+    AuthService,
+    UserRepository,
+    JwtStrategy,
+    UserService,
+    FriendService,
+    FriendRepository,
+    BlockRepository,
+    BlockService,
+    ChannelRepository,
+    ChannelUserRepository,
+    ChannelService,
+  ],
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/configs/jwt.config.ts
+++ src/configs/jwt.config.ts
@@ -4,3 +4,8 @@ export const JwtModuleConfig: JwtModuleOptions = {
   secret: process.env.JWT_SECRET,
   signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
 };
+
+export const RefreshJwtModuleConfig: JwtModuleOptions = {
+  secret: process.env.JWT_REFRESH_SECRET,
+  signOptions: { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN },
+};
