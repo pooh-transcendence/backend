587e5a4c3579ef2eed17779a08ca7c9d34e49a88
yubinquitous <yb9854@naver.com>
2023-07-25 01:25:56 +0900

    [FEAT] isFriend 추가 및 getFriendListByUserId user 디렉토리로 이동

--- src/friend/friend.service.ts
+++ src/friend/friend.service.ts
@@ -3,7 +3,7 @@ import { FriendEntity } from './friend.entity';
 import { FriendRepository } from './friend.respository';
 import { UserRepository } from 'src/user/user.repository';
 import { FriendDto } from './friend.dto';
-import { UserEntity } from 'src/user/user.entity';
+import { UserProfileDto } from 'src/user/user.dto';
 
 @Injectable()
 export class FriendService {
@@ -25,24 +25,20 @@ export class FriendService {
     return await this.friendRepository.createFriend(createFriendDto);
   }
 
-  async getFriendByFromId(id: number): Promise<{ to: number }[]> {
-    return await this.friendRepository.getFriendByFromId(id);
+  async getFriendListByFromId(id: number): Promise<{ to: number }[]> {
+    return await this.friendRepository.getFriendListByFromId(id);
   }
 
-  async getFriendByToId(id: number): Promise<{ from: number }[]> {
-    return await this.friendRepository.getFriendByToId(id);
-  }
-
-  async getFriendListByUserId(userId: number): Promise<UserEntity[]> {
-    const friendId = await this.getFriendByFromId(userId);
-    const friends = [];
-    for (const id of friendId) {
-      friends.push(await this.userRepository.getUserByUserId(id.to));
-    }
-    return friends;
+  async getFriendListByToId(id: number): Promise<{ from: number }[]> {
+    return await this.friendRepository.getFriendListByToId(id);
   }
 
   async deleteFriend(createFriendDto: FriendDto) {
     await this.friendRepository.deleteFriend(createFriendDto);
+  }
+
+  async isFriend(from: number, to: number): Promise<boolean> {
+    const friend = await this.friendRepository.findOneBy({ from, to });
+    return friend ? true : false;
   }
 }
